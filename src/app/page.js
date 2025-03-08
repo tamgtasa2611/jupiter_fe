a0b46bd8065517da9032731328a4462be7580bdb
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-03-08 14:02:56 +0700

    ant v5 support react 19

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-'use client';
+"use client";
+import "@ant-design/v5-patch-for-react-19";
 
-import { Layout } from 'antd';
-import Header from './components/customer/Header';
-import HeroBanner from './components/customer/HeroBanner';
-import Features from './components/customer/Features';
-import ProductShowcase from './components/customer/ProductShowcase';
-import Testimonials from './components/customer/Testimonials';
-import Newsletter from './components/customer/Newsletter';
-import Footer from './components/customer/Footer';
+import { Layout } from "antd";
+import Header from "./components/customer/Header";
+import HeroBanner from "./components/customer/HeroBanner";
+import Features from "./components/customer/Features";
+import ProductShowcase from "./components/customer/ProductShowcase";
+import Testimonials from "./components/customer/Testimonials";
+import Newsletter from "./components/customer/Newsletter";
+import Footer from "./components/customer/Footer";
 
 const { Content } = Layout;
 
